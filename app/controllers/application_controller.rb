49bb2dd6692d9dfa85453fa18d9a494b7c7ee1ce
Michael Nicosia <michael.nicosia@RACKSPACE.COM>
2018-04-18 23:32:01 -0500

    add /games.json route with no filtering/limits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_json_request
+
+  def ensure_json_request
+    head :not_acceptable unless request.format == :json
+  end
 end
