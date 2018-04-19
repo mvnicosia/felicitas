f3b5851cbeb5c4677def5345fdd528965a1a484e
Michael Nicosia <michael.nicosia@RACKSPACE.COM>
2018-04-18 22:25:29 -0500

    oops type -> game_type

--- db/migrate/20180419024636_create_games.rb
+++ db/migrate/20180419024636_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games, id: :uuid do |t|
-      t.string :type
+      t.string :game_type
       t.boolean :joinable
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2018_04_19_024636) do
   enable_extension "uuid-ossp"
 
   create_table "games", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "type"
+    t.string "game_type"
     t.boolean "joinable"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
