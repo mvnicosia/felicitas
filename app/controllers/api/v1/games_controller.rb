550743f6f0e3b55314dcd7ad76110425abe2a5ae
Michael Nicosia <michael.nicosia@RACKSPACE.COM>
2018-05-10 22:16:00 -0500

    add the ability to create a game record
    disabled auth for now

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -3,6 +3,12 @@ class Api::V1::GamesController < ApplicationController
   def index
     render json: Game.all
   end
+
   def create
+    game_type = params[:game_type]
+    joinable = params[:joinable]
+    new_game = Game.new(game_type: game_type, joinable: joinable)
+    new_game.save!
+    render json: new_game
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :ensure_json_request
+  skip_before_action :verify_authenticity_token
 
   def ensure_json_request
     head :not_acceptable unless request.format == :json
